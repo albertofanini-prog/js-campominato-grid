bb58f3f8a880ebc76474bafa948045c29c0217d7
albertofanini-prog <albertofanini@outlook.com>
2022-02-15 14:31:19 +0100

    2nd Commit - Spiegazione in italiano

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('prova')
+//console.log('prova')
+
+//Impostare una griglia 10x10
+//Inserire i numeri:
+    //Difficoltà 1 => Tra 1 e 100
+    //Difficoltà 2 => Tra 1 e 81
+    //Difficoltà 3 => Tra 1 e 49
+//L'utente indica un livello di difficoltà in base al quale viene generata una griglia
