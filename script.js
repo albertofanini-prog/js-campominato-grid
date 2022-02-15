052b4f288e9605bfa35630b4cfa0240c4fc8668d
albertofanini-prog <albertofanini@outlook.com>
2022-02-15 15:34:07 +0100

    4th Commit - JS Grid al click

--- script.js
+++ script.js
@@ -6,3 +6,45 @@
     //Difficoltà 2 => Tra 1 e 81
     //Difficoltà 3 => Tra 1 e 49
 //L'utente indica un livello di difficoltà in base al quale viene generata una griglia
+
+
+//Variabili
+
+//Nominare il click del bottone
+let play = document.querySelector('.play');
+
+//Nominare i livelli di difficoltà
+let difficult = document.getElementById('difficoltà');
+
+//Nominare il contenitore dei numeri
+let grid = document.querySelector('.grid');
+
+
+//Generare i quadrati
+function squareGen(rows, columns){
+    for(let i= 0; i < rows; i++){
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.append(+1);
+        grid.appendChild(square);
+    }
+}
+
+
+//Renderli visibili
+play.addEventListener('click', function(){
+    switch(difficult.value){
+        case 'easy':
+            squareGen(100, 10);
+            break;
+
+        case 'medium':
+            squareGen(81,9);
+            break;
+
+        case 'crazy':
+            squareGen(49,7);
+            break;
+    }
+
+})
